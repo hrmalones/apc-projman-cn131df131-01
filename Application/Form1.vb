37104ba93f64eaa467a6d8047c62a0b3060cf337
hrmalones <hrmalones@gmail.com>
2016-04-15 13:06:47 -0700

    Update Form1.vb

--- Application/Form1.vb
+++ Application/Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'SHCSystemDataSet1.PersonalInformation' table. You can move, or remove it, as needed.
-        Me.PersonalInformationTableAdapter.Fill(Me.SHCSystemDataSet1.PersonalInformation)
-        'TODO: This line of code loads data into the 'SHCSystemDataSet1.HIVVCT' table. You can move, or remove it, as needed.
-        Me.HIVVCTTableAdapter.Fill(Me.SHCSystemDataSet1.HIVVCT)
-        'TODO: This line of code loads data into the 'SHCSystemDataSet1.STIConsultation' table. You can move, or remove it, as needed.
-        Me.STIConsultationTableAdapter.Fill(Me.SHCSystemDataSet1.STIConsultation)
-        'TODO: This line of code loads data into the 'SHCSystemDataSet1.HealthCertificate' table. You can move, or remove it, as needed.
-        Me.HealthCertificateTableAdapter.Fill(Me.SHCSystemDataSet1.HealthCertificate)
+        'TODO: This line of code loads data into the 'eSHCDataSet1.PersonalInformation' table. You can move, or remove it, as needed.
+        Me.PersonalInformationTableAdapter.Fill(Me.eSHCDataSet1.PersonalInformation)
+        'TODO: This line of code loads data into the 'eSHCDataSet1.HIVVCT' table. You can move, or remove it, as needed.
+        Me.HIVVCTTableAdapter.Fill(Me.eSHCDataSet1.HIVVCT)
+        'TODO: This line of code loads data into the 'eSHCDataSet1.STIConsultation' table. You can move, or remove it, as needed.
+        Me.STIConsultationTableAdapter.Fill(Me.eSHCDataSet1.STIConsultation)
+        'TODO: This line of code loads data into the 'eSHCDataSet1.HealthCertificate' table. You can move, or remove it, as needed.
+        Me.HealthCertificateTableAdapter.Fill(Me.eSHCDataSet1.HealthCertificate)
 
         Me.ReportViewer1.RefreshReport()
        
